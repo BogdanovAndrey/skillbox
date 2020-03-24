4d148e61bd09e843d578c9b2bfd6f03dac47187a
BogdanovAndrey <51883189+BogdanovAndrey@users.noreply.github.com>
2020-03-24 21:44:31 +0300

    Module 7, task 2, part 2, one line function

--- 07_AdvancedOOPFeatures/Airport/src/Main.java
+++ 07_AdvancedOOPFeatures/Airport/src/Main.java
@@ -9,17 +9,14 @@ import java.util.Comparator;
 public class Main {
 
     public static void main(String[] args) {
-        Airport airport = Airport.getInstance();
+
         LocalDateTime testTime = LocalDateTime.now();
 
-        airport.getTerminals().forEach(terminal -> terminal.getFlights().stream().
-                filter(flight -> {
-                    LocalDateTime flightTime = LocalDateTime.ofInstant(flight.getDate().toInstant(), ZoneId.systemDefault());
-                    return flight.getType() == Flight.Type.DEPARTURE &&
-                            flightTime.isAfter(testTime) && flightTime.isBefore(testTime.plusHours(2));
-                }).sorted(Comparator.comparing(Flight::getDate)).forEach(flight -> {
-            System.out.println("Aircraft type: " + flight.getAircraft().getModel());
-            System.out.println("Departure time: " + flight.getDate());
-        }));
+        Airport.getInstance().getTerminals().stream().flatMap(terminal -> terminal.getFlights().stream())
+                .filter(flight -> flight.getType() == Flight.Type.DEPARTURE
+                        && testTime.isBefore(LocalDateTime.ofInstant(flight.getDate().toInstant(), ZoneId.systemDefault()))
+                        && testTime.plusHours(2).isAfter(LocalDateTime.ofInstant(flight.getDate().toInstant(), ZoneId.systemDefault())))
+                .sorted(Comparator.comparing(Flight::getDate))
+                .forEach(System.out::println);
     }
 }
