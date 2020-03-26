9dabb0315fb3c0c8c4166ae7c2ae5ec3da1dcaec
BogdanovAndrey <51883189+BogdanovAndrey@users.noreply.github.com>
2020-03-26 21:32:18 +0300

    Module 8, task 1

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
@@ -1,33 +1,53 @@
+import java.util.EmptyStackException;
 import java.util.HashMap;
 
-public class CustomerStorage
-{
+public class CustomerStorage {
     private HashMap<String, Customer> storage;
 
-    public CustomerStorage()
-    {
+    public CustomerStorage() {
         storage = new HashMap<>();
     }
 
-    public void addCustomer(String data)
-    {
+    public void addCustomer(String data) {
+
         String[] components = data.split("\\s+");
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Wrong format. Example:\n" +
+                    "add Василий Петров vasily.petrov@gmail.com +79215637722");
+        }
         String name = components[0] + " " + components[1];
+        checkMail(components[2]);
+        checkNumber(components[3]);
         storage.put(name, new Customer(name, components[3], components[2]));
     }
 
-    public void listCustomers()
-    {
+    public void listCustomers() {
         storage.values().forEach(System.out::println);
     }
 
-    public void removeCustomer(String name)
-    {
+    public void removeCustomer(String name) {
+        if (storage.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        if (!storage.containsKey(name)) {
+            throw new IllegalArgumentException("There is no such entry.");
+        }
         storage.remove(name);
     }
 
-    public int getCount()
-    {
+    public int getCount() {
         return storage.size();
+    }
+
+    private void checkNumber(String num) {
+        if (!num.matches("\\+7\\d{10}")) {
+            throw new IllegalArgumentException("Wrong number entry.");
+        }
+    }
+
+    private void checkMail(String mail) {
+        if (!mail.matches("[\\w.]+@\\w+\\.[a-z]+")) {
+            throw new IllegalArgumentException("Wrong email entry.");
+        }
     }
 }
