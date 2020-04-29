d7621bb421bcc58fa5272fbb20de91fa126142da
Bogdanov Andrey <bogd-merz@yandex.ru>
2020-04-29 11:46:28 +0300

    Module 9, task 4, folder check and gif support added

--- 09_FilesAndNetwork/InternetImageSaver/src/main/java/Saver.java
+++ 09_FilesAndNetwork/InternetImageSaver/src/main/java/Saver.java
@@ -9,6 +9,7 @@ import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
@@ -19,10 +20,17 @@ public class Saver {
 
     public static void main(String[] args) {
         System.out.println("Введите папку для сохранения изображения:");
-        System.out.print(">> ");
-        String path = new Scanner(System.in).nextLine();
-
+        String path = null;
+        boolean pathIsOk = false;
         try {
+            while (!pathIsOk) {
+                System.out.print(">> ");
+                path = new Scanner(System.in).nextLine();
+                pathIsOk = pathCheck(path);
+                if (!pathIsOk) {
+                    System.out.println("Указан не верный пусть или папка не пуста. Проверьте ввод");
+                }
+            }
             Document doc = Jsoup.connect(SITE).get();
             Elements img = doc.getElementsByTag("img");
             if (!Files.exists(Paths.get(path))) {
@@ -39,21 +47,32 @@ public class Saver {
         }
     }
 
-    public static void copyLink(String dest, String baseName, String src) {
+    private static void copyLink(String dest, String baseName, String src) {
         try {
-            if (src.contains("http")) {
-                if (!dest.endsWith("/")) {
-                    dest += "/";
-                }
-                URL path = new URL(src);
-                String ending = src.substring(src.lastIndexOf('.'));
-                ReadableByteChannel readableByteChannel = Channels.newChannel(path.openStream());
-                FileOutputStream fileOutputStream = new FileOutputStream(dest + baseName + ending);
-                fileOutputStream.getChannel()
-                        .transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+            String ending;
+            if (!src.contains("https:")) {
+                src = "https:" + src;
+                ending = ".gif";
+            } else {
+                ending = src.substring(src.lastIndexOf('.'));
             }
+            if (!dest.endsWith("/")) {
+                dest += "/";
+            }
+            URL path = new URL(src);
+            ReadableByteChannel readableByteChannel = Channels.newChannel(path.openStream());
+            FileOutputStream fileOutputStream = new FileOutputStream(dest + baseName + ending);
+            fileOutputStream.getChannel()
+                    .transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    private static boolean pathCheck(String path) throws IOException {
+        return path != null &&
+                !path.isEmpty() &&
+                Files.isDirectory(Path.of(path)) &&
+                Files.walk(Path.of(path)).count() == 1;
     }
 }
