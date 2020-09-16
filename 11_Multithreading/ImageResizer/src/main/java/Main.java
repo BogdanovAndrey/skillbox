fdd534297ab07b6226f73bfbe772090a97e5cf33
Андрей <bogd-merz@yandex.ru>
2020-09-16 21:12:43 +0300

    Module 11, task 1, complete both methods

--- 11_Multithreading/ImageResizer/src/main/java/Main.java
+++ 11_Multithreading/ImageResizer/src/main/java/Main.java
@@ -1,7 +1,9 @@
-import util.FineImageResizer;
+import org.imgscalr.Scalr;
 import util.FolderType;
-import util.RoughImageResizer;
+import util.ImageResizer;
+import util.ResizeMethod;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -17,30 +19,57 @@ public class Main {
         String roughDstFolder = "resources/output/rough";
         String fineDstFolder = "resources/output/fine";
 
-        final int newWidth = 300;
         final int AVAILABLE_CORE_NUM = Runtime.getRuntime().availableProcessors();
         final String inputMsg = "Введите исходную папку:";
         final String outputMsg = "Введите конечную папку:";
-        final String typeMsg = "Выберете тип операции:\n\t R - грубое маштабирование;" +
-                "\n\t F - масштабирование, с сохранением качества";
-
+        final String fine = "F";
+        final String rough = "R";
+        final String typeMsg = "Выберете тип операции:\n\t" + rough + "- грубое масштабирование;" +
+                "\n\t" + fine + "- масштабирование, с сохранением качества";
+        final String widthMsg = "Введите новое значение ширины картинки";
         Scanner input = new Scanner(System.in);
 
+
         try {
-            File srcDir = new File(validateFolder(input,inputMsg, FolderType.INPUT));
+            File srcDir = new File(validateFolder(input, inputMsg, FolderType.INPUT));
 
             File[] files = srcDir.listFiles();
 
             String dstFolder = validateFolder(input, outputMsg, FolderType.OUTPUT);
 
-            String operationType = getFolder(input, typeMsg);
-            switch (operationType){
-                case "F":{
-                    roughResize(files, dstFolder, newWidth, AVAILABLE_CORE_NUM);
+            int newWidth = Integer.parseInt(getInputString(input, widthMsg));
+
+            String operationType = getInputString(input, typeMsg);
+
+
+            switch (operationType) {
+                case "F": {
+                    ResizeMethod method = (image, height, width) -> Scalr.resize(
+                            image,
+                            Scalr.Method.ULTRA_QUALITY,
+                            width,
+                            height);
+                    resizeFiles(files, dstFolder, newWidth, AVAILABLE_CORE_NUM, method);
                     break;
                 }
-                case "R":{
-                    fineResize(files, dstFolder, newWidth,AVAILABLE_CORE_NUM);
+                case "R": {
+                    ResizeMethod method = (image, height, width) -> {
+                        BufferedImage newImage = new BufferedImage(
+                                width, height, BufferedImage.TYPE_INT_RGB
+                        );
+
+                        int widthStep = image.getWidth() / width;
+                        int heightStep = image.getHeight() / height;
+
+                        for (int x = 0; x < width; x++) {
+                            for (int y = 0; y < height; y++) {
+                                int rgb = image.getRGB(x * widthStep, y * heightStep);
+                                newImage.setRGB(x, y, rgb);
+                            }
+                        }
+                        return newImage;
+                    };
+                    resizeFiles(files, dstFolder, newWidth, AVAILABLE_CORE_NUM, method);
                     break;
                 }
                 default:{
@@ -54,17 +83,9 @@ public class Main {
         }
     }
 
-
-
-    private static void roughResize(File[] files, String dstFolder, int newWidth, int availableCoreNum) throws NullPointerException {
+    private static void resizeFiles(File[] files, String dstFolder, int newWidth, int availableCoreNum, ResizeMethod exp) throws NullPointerException {
         ArrayList<File[]> splittedFiles = splitFiles(availableCoreNum, files);
-        splittedFiles.forEach(filesPart -> new Thread(new RoughImageResizer(filesPart, dstFolder, newWidth)).start());
-        System.out.printf("Создано потоков: %d.\n", splittedFiles.size());
-    }
-
-    private static void fineResize(File[] files, String dstFolder, int newWidth, int availableCoreNum) throws NullPointerException {
-        ArrayList<File[]> splittedFiles = splitFiles(availableCoreNum, files);
-        splittedFiles.forEach(filesPart -> new Thread(new FineImageResizer(filesPart, dstFolder, newWidth)).start());
+        splittedFiles.forEach(filesPart -> new Thread(new ImageResizer(filesPart, dstFolder, newWidth, exp)).start());
         System.out.printf("Создано потоков: %d.\n", splittedFiles.size());
     }
 
@@ -79,16 +100,22 @@ public class Main {
         }
         return splittedFiles;
     }
-    private static String getFolder (Scanner input, String msg) throws IOException {
+
+    private static String getInputString(Scanner input, String msg) {
         System.out.println(msg);
         System.out.print(">>");
         return input.nextLine();
     }
 
     private static String validateFolder(Scanner input, String msg, FolderType type) throws IllegalArgumentException, IOException {
-        Path folder = Path.of(getFolder(input, msg));
+        Path folder = Path.of(getInputString(input, msg));
         if (!Files.exists(folder)) {
-            throw new IllegalArgumentException("Папки не существует");
+            if (type == FolderType.OUTPUT) {
+                Files.createDirectory(folder);
+                System.out.println("Папка создана");
+            } else {
+                throw new IllegalArgumentException("Папки не существует");
+            }
         }
         if (!Files.isDirectory(folder)) {
             throw new IllegalArgumentException("Введеный путь не является папкой");
