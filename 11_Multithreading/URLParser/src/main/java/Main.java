ef2f0094f746c808d38fadb4a148dd6f9b819fe7
Андрей <bogd-merz@yandex.ru>
2020-10-08 06:02:14 +0300

    08_10_2020

--- 11_Multithreading/URLParser/src/main/java/Main.java
+++ 11_Multithreading/URLParser/src/main/java/Main.java
@@ -1,15 +1,9 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-import util.TreeMapper;
+import util.SiteMapper;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -17,14 +11,13 @@ import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     final static Path file = Path.of("src/main/resources/output.txt").toAbsolutePath();
-    final static String mainPage = "https://lenta.ru/";
-
+    final static String mainPage = "https://lenta.ru";
     public static void main(String[] args) {
         try {
-
             SortedSet<String> siteMap = Collections.synchronizedSortedSet(new TreeSet<String>());
 
-            new ForkJoinPool().invoke(new TreeMapper(mainPage, siteMap));
+            new ForkJoinPool().invoke(new SiteMapper(mainPage, siteMap));
+
 
             Files.deleteIfExists(file);
             Files.createFile(file);
---
+++ 11_Multithreading/URLParser/src/main/java/util/SiteMapper.java
@@ -0,0 +1,69 @@
+package util;
+
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.concurrent.RecursiveAction;
+
+public class SiteMapper extends RecursiveAction {
+    SortedSet<String> links;
+    final String url;
+
+    public SiteMapper(String url, SortedSet<String> links) {
+        this.url = url;
+        this.links = links;
+    }
+
+
+    @Override
+    protected void compute() {
+
+        try {
+            Thread.sleep(150);
+            Document page = Jsoup.connect(url).get();
+            Elements rawLinks = page.select("a[abs:href]");
+
+            boolean newStringsAdded = false;
+            for (Element el : rawLinks) {
+                String link = el.attr("abs:href");
+                if (validLink(link)) {
+                    newStringsAdded = links.add(link);
+                }
+            }
+            if (!newStringsAdded) {
+                return;
+            }
+        } catch (HttpStatusException ex) {
+            System.out.println("Ошибка " + ex.getStatusCode() + " на странице " + ex.getUrl());
+            links.remove(url);
+            return;
+        } catch (IOException | InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+
+        List<SiteMapper> subTasks = new LinkedList<>();
+
+        for (String link : links) {
+            SiteMapper task = new SiteMapper(link, links);
+            task.fork(); // запустим асинхронно
+            subTasks.add(task);
+        }
+
+        for (SiteMapper task : subTasks) {
+            task.join();
+        }
+    }
+
+    private boolean validLink(String link) {
+        return (link.contains("https://lenta.ru")
+                || !);
+    }
+}
