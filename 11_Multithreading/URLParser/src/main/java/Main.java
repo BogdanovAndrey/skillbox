bf3183d418ed251930c7b2a79ed2933f5da96016
Андрей <bogd-merz@yandex.ru>
2020-10-10 18:54:40 +0300

    Module 11, task 3, complete

--- 11_Multithreading/URLParser/src/main/java/Main.java
+++ 11_Multithreading/URLParser/src/main/java/Main.java
@@ -4,43 +4,52 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.util.Collections;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     final static Path file = Path.of("src/main/resources/output.txt").toAbsolutePath();
-    final static Path file2 = Path.of("src/main/resources/output2.txt").toAbsolutePath();
-    final static String mainPage = "https://lenta.ru";
+    final static String mainPage = "https://skillbox.ru/";
     public static void main(String[] args) {
         try {
             Set<String> siteMap = new TreeSet<String>();
-            Set<String> siteMap2 = Collections.synchronizedSortedSet(new TreeSet<String>());
             new ForkJoinPool().invoke(new SiteMapper(mainPage, mainPage, siteMap));
 
+            printToFile(file, siteMap);
 
-            Files.deleteIfExists(file);
-            Files.createFile(file);
-            siteMap.forEach(s -> {
-                try {
-                    Files.writeString(file, s + "\n", StandardOpenOption.APPEND);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-            new ForkJoinPool().invoke(new SiteMapper(mainPage, mainPage, siteMap2));
-            Files.deleteIfExists(file2);
-            Files.createFile(file2);
-            siteMap.forEach(s -> {
-                try {
-                    Files.writeString(file2, s + "\n", StandardOpenOption.APPEND);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void printToFile(Path file, Set<String> siteMap) throws IOException {
+        Files.deleteIfExists(file);
+        Files.createFile(file);
+        siteMap.forEach(s -> {
+            try {
+                StringBuilder outStr = new StringBuilder();
+                outStr.append("\t".repeat(Math.max(0, countLevel(s)))).append(s).append("\n");
+                Files.writeString(file, outStr, StandardOpenOption.APPEND);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private static int countLevel(String link) {
+        char[] chars = link.toCharArray();
+        int level = 0;
+        for (char c : chars) {
+            if (c == '/') {
+                level++;
+            }
+        }
+        //Отнимаем два слеша из https://
+        level = level - 2;
+        if (chars[chars.length - 1] == '/') {
+            level--;
+        }
+        return level;
     }
 }
--- 11_Multithreading/URLParser/src/main/java/util/SiteMapper.java
+++ 11_Multithreading/URLParser/src/main/java/util/SiteMapper.java
@@ -10,22 +10,30 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.RecursiveAction;
 
+
 public class SiteMapper extends RecursiveAction {
+    //Базовый адрес
     final String domain;
-    Set<String> links;
+    //Общий список адресов
+    final Set<String> links;
+    //Текущий адрес
     final String url;
 
+
     public SiteMapper(String domain, String url, Set<String> links) {
-        this.domain = domain;
+        //Отрежем слэш для проверок
+        this.domain = trimTail(domain);
         this.url = url;
         this.links = links;
     }
 
-
     @Override
     protected void compute() {
+        //Список новых ссылок, чтобы избежать повторной проверки старых
+        Set<String> newLinks = new TreeSet<>();
 
         try {
             Thread.sleep(150);
@@ -34,30 +42,46 @@ public class SiteMapper extends RecursiveAction {
 
             boolean newStringsAdded = false;
             for (Element el : rawLinks) {
+                //Получаем абсолютный адрес ссылки
                 String link = el.attr("abs:href");
-                if (validLink(link)) {
-                    newStringsAdded = addLink(link, links);
+                //Если это базовый, пропускаем - добавим потом
+                if (link.equals(domain)) {
+                    continue;
+                }
+                //Добавим слэш для единообразия
+                link = addTail(link);
+                //Проверим содержит ли ссылка базовый адрес
+                //и не обрабатывали ли мы ее ранее
+                if (validLink(link)
+                        && !links.contains(link)) {
+                    //Новая ссылка нашлась
+                    newStringsAdded = true;
+                    //Запишем для дальнейшей обработки
+                    newLinks.add(link);
+                    System.out.println("Added " + link);
+                    //Добавим в список с учетом иерархии
+                    addLink(link, links);
                 }
             }
             if (!newStringsAdded) {
                 return;
             }
-        } catch (HttpStatusException ex) {
-            System.out.println("Ошибка " + ex.getStatusCode() + " на странице " + ex.getUrl());
-            links.remove(url);
-            return;
+        } catch (HttpStatusException ignored) {
+            //Пустные страницы игнорируем
         } catch (IOException | InterruptedException ex) {
             ex.printStackTrace();
         }
 
+        //System.out.printf("Добавлено %d ссылок\n",newLinks.size());
 
         List<SiteMapper> subTasks = new LinkedList<>();
-
-        for (String link : links) {
+        //Запускаем обработку новых ссылок
+        for (String link : newLinks) {
             SiteMapper task = new SiteMapper(domain, link, links);
-            task.fork(); // запустим асинхронно
+            task.fork();
             subTasks.add(task);
         }
+        //System.out.printf("\tЗапущено %d новых задач\n", subTasks.size());
 
         for (SiteMapper task : subTasks) {
             task.join();
@@ -66,22 +90,35 @@ public class SiteMapper extends RecursiveAction {
 
     private boolean validLink(String link) {
         return (link.startsWith(domain)
-                && !link.contains(".pdf")
-                && !link.contains(".xls"));
+                && !link.replaceFirst(domain, "").contains("."));
     }
 
-    private boolean addLink(String link, Set<String> map) {
-        boolean update = false;
-        String tail = link.replaceFirst(domain, "");
-        String[] subLinks = tail.split("/");
-        StringBuilder newLink = new StringBuilder(domain);
-        for (String s : subLinks) {
-            if (!s.isEmpty()) {
-                newLink.append(s);
-            }
-            newLink.append("/");
-            update = map.add(newLink.toString());
+    private void addLink(String link, Set<String> map) {
+        //Сохраняем исходную ссылку
+        map.add(link);
+        //Отрежем последний слэш
+        link = trimTail(link);
+
+        //Будем добавлять подуровни ссылки до тех пор,
+        // пока не дойдем до базового адреса
+        while (!trimTail(link).equals(domain) || !link.equals(domain)) {
+            link = link.substring(0, link.lastIndexOf("/"));
+            map.add(link + "/");
         }
-        return update;
+
+    }
+
+    private String trimTail(String link) {
+        if (link.endsWith("/")) {
+            link = link.substring(0, link.lastIndexOf("/"));
+        }
+        return link;
+    }
+
+    private String addTail(String link) {
+        if (!link.endsWith("/")) {
+            link = link.concat("/");
+        }
+        return link;
     }
 }
